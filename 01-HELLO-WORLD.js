1911ff734fa7ef9d35e2db9141f6f43f38f1137e
Gia Bao <hoanggiabao131012vn@gmail.com>
2026-03-07 09:00:56 +0700

    JS-EXERCIES

---
+++ 01-HELLO-WORLD.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+console.log("Hello, My friends!");
+console.log("I love JS so much!");
---
+++ 02-ADD-NUMBER.js
@@ -0,0 +1,7 @@
+a = 6.7;
+b = 6.1;
+
+console.log(a + b);
+
+c = a + b
+console.log(`${a} + ${b} = ${c}`)
