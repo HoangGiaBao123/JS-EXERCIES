3cf3de960fac0962ebb59bcef0e8a9ef4d568eb2
Gia Bao <hoanggiabao131012vn@gmail.com>
2026-03-29 17:53:52 +0700

    03-NUMBER-CHECKER

---
+++ 03-NUMEBR-CHECKER.js
@@ -0,0 +1,11 @@
+function numberChecker(number) {
+    if(number >= 10) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+console.log(numberChecker(67));
+console.log(numberChecker(6.7));
